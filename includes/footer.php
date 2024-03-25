9e4d3f7bd467a59ddb7363c2cedec8433660a67b
koshikawa-webkadai1 <0035web@gmail.com>
2024-03-25 14:06:56 +0900

    imgのみ追加

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link rel="stylesheet" href="">
+
+
+</head>
 <body>
 
-</body>
 
+<footer>
+
+
+
+
+
+</footer>
+    
+</body>
 </html>
