6c5211e63d9ea6c3da1eea6b47e28a0ef9099da1
ogi226 <h.ogino0622@gmail.com>
2024-03-27 14:28:11 +0900

    荻野担当部分各種更新しました

--- card-complete.php
+++ card-complete.php
@@ -1,14 +1,33 @@
-<?php require 'includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="common/css/reset.css">
+  <link rel="stylesheet" href="common/css/customer.css">
+  <title>Card-complete | donuts-site</title>
+</head>
 
+<body>
+  <main>
+    <img src="common/images/logo_sp.png" alt="ロゴ">
+    <h1>カード情報登録完了</h1>
+    <div class="content">
+      <div class="content_inner complete_content_inner textalign_center">
+        <p class="message">クレジットカード情報を登録しました。</p>
+        <div class="textalign_center">
+          <a class="memo" href="">購入手続きを続ける</a>
+        </div>
+      </div><!-- /content_inner -->
+    </div><!-- /content -->
 
+  </main>
+</body>
 
-
-
-
-
-
-
-
+</html>
 
 <?php require 'includes/footer.php'; ?>
