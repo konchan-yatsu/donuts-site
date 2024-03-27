0d15ad70e9deba4e9408ba2bddef61aec3d16616
koshikawa-webkadai1 <0035web@gmail.com>
2024-03-27 15:33:18 +0900

    header　最大幅　調整

--- includes/header.php
+++ includes/header.php
@@ -10,85 +10,88 @@
 
 <header>
 
-<div id="header_top">
 
-<a class="logo" href="index.php"><img src="common/images/logo_sp.png" alt="ハンバーガーストア-ロゴ"></a>
-
-
-<nav>
-
-<!-- メニューボタン -->
-<div class="nav">
-  <div class="nav_icon_bg">
-  </div>
-  <input type="checkbox" class="menu_botan" id="menu_botan">
-  <label for="menu_botan" class="menu_icon">
-
-    <span class="nav_icon"></span>
-  </label>
-
-  <ul class="nav_menu">
-
-    <li><img src="common/images/logo_sp.png" alt="C.C.Donuts画像"></li>
-
-    <!-- メニュー一覧 -->
-    <div>
-    <li><a href="index.php">top</a></li>
-
-    <hr>
-
-    <li><a href="product.php">商品一覧</a></li>
-
-  <hr>
-
-    <li><a href="#">よくある質問</a></li>
-
-    <hr>
-
-    <li><a href="#">問い合わせ</a></li>
-
-    <hr>
-
-    <li><a href="#">当サイトのポリシー</a></li>
-
-    <hr>
-
+  
+  <div id="header_top">
+  
+  <a class="logo" href="index.php"><img src="common/images/logo_sp.png" alt="ハンバーガーストア-ロゴ"></a>
+  
+  <div id=header_max>
+  <nav>
+  
+  <!-- メニューボタン -->
+  <div class="nav">
+    <div class="nav_icon_bg">
     </div>
+    <input type="checkbox" class="menu_botan" id="menu_botan">
+    <label for="menu_botan" class="menu_icon">
+  
+      <span class="nav_icon"></span>
+    </label>
+  
+    <ul class="nav_menu">
+  
+      <li><img src="common/images/logo_sp.png" alt="C.C.Donuts画像"></li>
+  
+      <!-- メニュー一覧 -->
+      <div>
+      <li><a href="index.php">top</a></li>
+  
+      <hr>
+  
+      <li><a href="product.php">商品一覧</a></li>
+  
+    <hr>
+  
+      <li><a href="#">よくある質問</a></li>
+  
+      <hr>
+  
+      <li><a href="#">問い合わせ</a></li>
+  
+      <hr>
+  
+      <li><a href="#">当サイトのポリシー</a></li>
+  
+      <hr>
+  
+      </div>
+    </ul>
+  </div>
+  
+  </nav>
+  </div>
+
+
+  <!-- <ul> -->
+  <!-- <li><a class="login" href="../index.html"><img src="common/images/icon_login_sp.svg" alt="ログインアイコン"></a></li> -->
+  <!-- <li><a class="cart" href="../index.html"><img src="common/images/icon_cart_sp.svg" alt="カートアイコン"></a></li> -->
+  <!-- </ul> -->
+  
+  <a class="login" href="../index.html"><img src="common/images/icon_login_sp.svg" alt="ログインアイコン"></a>
+  
+  <a class="cart" href="../index.html"><img src="common/images/icon_cart_sp.svg" alt="カートアイコン"></a>
+  
+  </div>
+  
+  <div id="header_search">
+  
+  
+  <ul>
+    <li>
+    <button type="submit"><img src="common/images/icon_search_sp.svg" ></button>
+    </li>
+    <li id=text>
+    <input type="text">
+    </li>
+  
   </ul>
-</div>
+  
+ 
 
-
-
-
-</nav>
-
-<!-- <ul> -->
-<!-- <li><a class="login" href="../index.html"><img src="common/images/icon_login_sp.svg" alt="ログインアイコン"></a></li> -->
-<!-- <li><a class="cart" href="../index.html"><img src="common/images/icon_cart_sp.svg" alt="カートアイコン"></a></li> -->
-<!-- </ul> -->
-
-<a class="login" href="../index.html"><img src="common/images/icon_login_sp.svg" alt="ログインアイコン"></a>
-
-<a class="cart" href="../index.html"><img src="common/images/icon_cart_sp.svg" alt="カートアイコン"></a>
-
-</div>
-
-<div id="header_search">
-
-
-<ul>
-  <li>
-  <button type="submit"><img src="common/images/icon_search_sp.svg" ></button>
-  </li>
-  <li id=text>
-  <input type="text">
-  </li>
-
-</ul>
-
-</div>
 
 </header>
+
 
 
 
