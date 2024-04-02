5a1f2548b3c7e03a94051d1573db83dc7bfaf826
mai7-7 <zhenyix720@gmail.com>
2024-04-02 11:36:28 +0900

    更新

--- product.php
+++ product.php
@@ -15,46 +15,69 @@
 
 <body>
   <h1>商品一覧</h1>
-  <?php
-  $img1 = ['goodslist_cont1_no1_pc.png', 'goodslist_cont1_no2_pc.png', 'goodslist_cont1_no3_pc.png', 'goodslist_cont1_no4_pc.png', 'goodslist_cont1_no5_pc.png', 'goodslist_cont1_no6_pc.png'];
+  <div class="flex_content">
 
-  $img2 = ['goodslist_cont2_no1_pc.png', 'goodslist_cont2_no2_pc.png', 'goodslist_cont2_no3_pc.png', 'goodslist_cont2_no4_pc.png', 'goodslist_cont2_no5_pc.png', 'goodslist_cont2_no6_pc.png'];
 
-  $name1 = ['CCドーナツ 当店オリジナル（5個入り）', 'チョコレートデライト（5個入り）', 'キャラメルクリーム（5個入り）', 'プレーンクラシック
+
+    <?php
+    $img1 = ['goodslist_cont1_no1_pc.png', 'goodslist_cont1_no2_pc.png', 'goodslist_cont1_no3_pc.png', 'goodslist_cont1_no4_pc.png', 'goodslist_cont1_no5_pc.png', 'goodslist_cont1_no6_pc.png'];
+
+    $img2 = ['goodslist_cont2_no1_pc.png', 'goodslist_cont2_no2_pc.png', 'goodslist_cont2_no3_pc.png', 'goodslist_cont2_no4_pc.png', 'goodslist_cont2_no5_pc.png', 'goodslist_cont2_no6_pc.png'];
+
+    $name1 = ['CCドーナツ 当店オリジナル（5個入り）', 'チョコレートデライト（5個入り）', 'キャラメルクリーム（5個入り）', 'プレーンクラシック
   （5個入り）', '【新作】サマーシトラス（5個入り）', 'ストロベリークラッ
   シュ（5個入り）'];
 
-  $name2 = ['', '', '', '', '', ''];
+    $name2 = ['フルーツドーナツセット（12個入り）', 'フルーツドーナツセット（14個入り）', 'ベストセレクションボックス（4個入り）', 'チョコクラッシュボックス（7個入り）', 'クリームボックス（4
+  個入り）', 'クリームボックス（9
+  個入り）'];
 
-  $price1 = ['税込  ￥1,500', '税込  ￥1,600', '', '税込  ￥1,600', '税込  ￥1,500', '税込  ￥1,600', '税込  ￥1,800'];
+    $price1 = ['税込  ￥1,500', '税込  ￥1,600',  '税込  ￥1,600', '税込  ￥1,500', '税込  ￥1,600', '税込  ￥1,800'];
 
-  $price2 = ['', '', '', '', '', '', ''];
+    $price2 = ['税込  ￥3,500', '税込 ￥4,000', '税込 ￥1,200', '税込  ￥2,400', '税込  ￥1,400', '税込  ￥2,800'];
 
-  // 商品一覧の１つめ
-  foreach (array_map(null, $img1, $name1, $price1) as $key => [$col1, $n, $p]) {
-    $id = $key + 1;
-    echo <<<END
-    <a href="detail-1.php?id="$id">
-<p><img src="common/images/{$col1}" alt="商品画像"></p>
+    // 商品一覧の１つ
+    foreach (array_map(null, $img1, $name1, $price1) as $key => [$col1, $n, $p]) {
+      $id = $key + 1;
+      echo <<<END
+      <div class="flex_item">
+  <p><img src="common/images/{$col1}" alt="商品画像"></p>
 <p>{$n}</p>
+<div class="inner_flex">
 <p>{$p}</p>
-</a>
+<p><img src="common/images/heart.svg" alt="heart"></p>
+</div>
+<button class="btn_cart"><a href="#">カートに入れる</a></button>
+</div>
 END;
-  }
-  // 商品一覧の２つ目
-  foreach (array_map(null, $img2, $name2, $price2) as $key => [$col3, $n2, $p2]) {
-    $id = $key + 7;
-    echo <<<END
-  <a href="detail-1.php?id="$id">
-<p><img src="common/images/{$col3}" alt="商品画像"></p>
-<p>{$n2}</p>
-<p>{$p2}</p>
-</a>
+    }
+    ?>
+
+  </div>
+
+
+  <h1>バラエティセット</h1>
+  <div class="flex_content2">
+
+    <?php
+    // 商品一覧の２つ目
+    foreach (array_map(null, $img2, $name2, $price2) as $key => [$col3, $n2, $p2]) {
+      $id = $key + 7;
+      echo <<<END
+
+
+    <div class="flex_item">
+      <p><img src="common/images/{$col3}" alt="商品画像"></p>
+      <p>{$n2}</p>  
+      <p>{$p2}</p>
+      <p><img src="common/images/heart.svg" alt="heart"></p>
+      <button class="btn_cart"><a href="#">カートに入れる</a></button>
+    </div>
 END;
-  }
-
-  ?>
-
+    }
+    // var_dump($key);
+    ?>
+  </div>
 
 </body>
 
