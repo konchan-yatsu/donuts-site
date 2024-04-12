18c53809665708cf100766dccd8ac3f042a87f7c
koshikawa-webkadai1 <0035web@gmail.com>
2024-04-12 09:24:29 +0900

    カートチェンジのバグ修正

--- count_change.php
+++ count_change.php
@@ -52,36 +52,59 @@ $id=$_REQUEST['id'];
 
 $count = $_SESSION['product'][$id]['count'];
 
-if($_SESSION['product'][$id]['count'] > 0){
-// 数量が0より大きい場合　true
-// ??????????????????????????????????????????????
 
+
+
+
+
+
+
+if($_SESSION['product'][$id]['count'] > 0){
+// $_SESSION['product'][$id]['count']が0より大きい場合　true
 
   $count = $_SESSION['product'][$id]['count'];
+  // 変数$countに元の数を代入
 
   require 'includes/database.php';
+  // データベース読み込み
 
-    if($_REQUEST['add'] - $count < 1){
-        // ($_REQUEST['add'] - $count が0以下の場合　true
+
+
+
+
+
+
+
+    if($_REQUEST['add'] + $count < 1){
+        // ($_REQUEST['add'] + $count が1未満の場合　true
         
         $_SESSION['product'][$id]['count']=1;
+        // $_SESSION['product'][$id]['count']に「1」を代入
 
         $sql = $pdo->prepare('select * from product where id=?');
+        // product内のidと　Web内idが同じものを確認
+
         $sql->execute([$_REQUEST['id']]);
+        // データベースからいidを読み込み
         
         foreach ($sql as $cart) {
         
           $_SESSION['product'][$id] = [
           'name' => $cart['name'],
           'price' => $cart['price'],
-          'count' => $count
+          'count' => $_SESSION['product'][$id]['count']
           ];
+          // idが同じもののデータを読み込み
 
-    }
+        }
+
+        echo '<p class="id_name_no_cart">',$_SESSION['product'][$id]['name'],'の数量　1　に変更しました。</p>';
 
 
-  }
 
+
+    }else{
+      // ($_REQUEST['add'] + $count が2以上の場合　false
 
       $sql = $pdo->prepare('select * from product where id=?');
       $sql->execute([$_REQUEST['id']]);
@@ -93,45 +116,52 @@ if($_SESSION['product'][$id]['count'] > 0){
         'price' => $cart['price'],
         'count' => $count+$_REQUEST['add']
         ];
-  }
+        }
 
-  echo '<p class="id_name_no_cart">',$_SESSION['product'][$id]['name'],'の数量を変更しました。</p>';
-  echo '<hr>';
-  require 'cart.php';
-  echo '</main>';
+        echo '<p class="id_name_no_cart">',$_SESSION['product'][$id]['name'],'の数量を変更しました。</p>';
+    }
+
+
+
+
+
+
+
+
 
 
 
 
 }else {
-// 数量がマイナスの場合　false　
+  // 数量がマイナスの場合　false　
 
-$_SESSION['product'][$id]['count']=1;
+  $_SESSION['product'][$id]['count']=1;
 
-$count = $_SESSION['product'][$id]['count'];
+  $count = $_SESSION['product'][$id]['count'];
 
 
 
-$sql = $pdo->prepare('select * from product where id=?');
-$sql->execute([$_REQUEST['id']]);
+  $sql = $pdo->prepare('select * from product where id=?');
+  $sql->execute([$_REQUEST['id']]);
     
-foreach ($sql as $cart) {
+  foreach ($sql as $cart) {
     
-  $_SESSION['product'][$id] = [
-    'name' => $cart['name'],
-    'price' => $cart['price'],
-    'count' => $count
-  ];
+    $_SESSION['product'][$id] = [
+      'name' => $cart['name'],
+      'price' => $cart['price'],
+      'count' => $count
+      ];
+ 
+  }
+
+  echo '<p class="id_name_no_cart">',$_SESSION['product'][$id]['name'],'の数量を　1　に変更しました。</p>';
+
+
 }
 
-
-echo '<p class="id_name_no_cart">',$_SESSION['product'][$id]['name'],'の数量を　1　に変更しました。</p>';
 echo '<hr>';
 require 'cart.php';
 echo '</main>';
-
-
-}
 
   ?>
 
